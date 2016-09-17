6b209987a89d397c684e74dbe81fd62f7040120d
suzukisan22 <getty2017@gmail.com>
2016-09-17 13:47:10 +0000

    indexとnewメソッドの実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,20 @@
 class TweetsController < ApplicationController
   def index
+    @tweet = Exam01.all
   end
+  
+  def new
+    @tweet = Exam01.new
+  end
+  
+  def create
+    @tweet = Exam01.new(tweet_params)
+    @tweet.save
+    redirect_to root_path, notice: 'tweetを投稿しました'
+  end
+  
+  private
+    def tweet_params
+      params.require(:exam01).permit(:content)
+    end
 end
