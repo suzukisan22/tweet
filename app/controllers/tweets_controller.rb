93508eb675fbfb11abf2aa22378e6b7bdde17445
Ryota <ryt.0081@gmail.com>
2017-03-19 16:17:44 +0900

    set filter at tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,11 @@
 class TweetsController < ApplicationController
+  skip_before_filter :require_login, only: [:index, :show]
   before_action :set_tweet, only: [:edit, :update, :destroy]
-  
+
   def index
     @tweet = Exam01.order(updated_at: :desc)
   end
-  
+
   def new
     if params[:back]
       @tweet = Exam01.new(tweet_params)
@@ -12,39 +13,39 @@ class TweetsController < ApplicationController
       @tweet = Exam01.new
     end
   end
-  
+
   def confirm
     @tweet = Exam01.new(tweet_params)
     render :new if @tweet.invalid?
   end
-  
+
   def create
     @tweet = Exam01.new(tweet_params)
     if @tweet.save
       redirect_to root_path, notice: 'ツイートを投稿しました'
-    else 
+    else
       render :new
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     @tweet.update(tweet_params)
     redirect_to root_path, notice: 'ツイートを更新しました'
   end
-  
+
   def destroy
     @tweet.destroy
     redirect_to root_path, notice: 'ツイートを削除しました'
   end
-  
+
   private
     def tweet_params
       params.require(:exam01).permit(:content)
     end
-    
+
     def set_tweet
       @tweet = Exam01.find(params[:id])
     end
